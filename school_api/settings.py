d7be4279315b92f589df2eb8d932679f1b39be99
hartnerk <hartnerk@gmail.com>
2021-04-14 17:05:15 -0400

    Updated Debug to False For deployment

--- school_api/settings.py
+++ school_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7z!g)%1nm%c3i54b=xt(%#xx8xe(tz36$q*@g52*ek$et6*c-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
